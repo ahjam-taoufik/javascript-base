bb9aebc624e2420393e509236b4a9e69c8698fe9
taoufik ahjam <taoufikahjam@gmail.com>
2021-12-30 01:39:18 +0100

    aperator and logic

---
+++ Operator & Logical.js
@@ -0,0 +1,14 @@
+/*
+   Operator & Logical Or
+*/
+
+console.log(Boolean(100));
+console.log(Boolean(-100));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(null));
+
+let price = 0;
+
+console.log(`The Price Is ${price || 200}`);
+console.log(`The Price Is ${price ?? 200}`);
