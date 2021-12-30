98082784bc2958d80c1c79b70b83e895f8a3de62
taoufik ahjam <taoufikahjam@gmail.com>
2021-12-30 01:49:32 +0100

    Nested function

---
+++ 15-Nested function.js
@@ -0,0 +1,48 @@
+/*
+  Function
+  - Function Inside Function
+  - Return Function
+*/
+
+// Example 1
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    message = `${message} ${fName} ${lName}`;
+  }
+  concatMsg();
+  return message;
+}
+
+console.log(sayMessage("taoufik", "Mohamed"));
+
+// Example 2
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    return `${message} ${fName} ${lName}`;
+  }
+  return concatMsg();
+}
+
+console.log(sayMessage("taoufik", "Mohamed"));
+
+// Example 3
+
+function sayMessage(fName, lName) {
+  let message = `Hello`;
+  // Nested Function
+  function concatMsg() {
+    function getFullName() {
+      return `${fName} ${lName}`;
+    }
+    return `${message} ${getFullName()}`;
+  }
+  return concatMsg();
+}
+
+console.log(sayMessage("taoufik", "Mohamed"));
