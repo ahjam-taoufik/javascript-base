27a97b5adc113dac36ca8ac71104f27a3e2b806a
taoufik ahjam <taoufikahjam@gmail.com>
2021-12-30 01:42:18 +0100

    conditional operator

---
+++ Conditional Operator.js
@@ -0,0 +1,10 @@
+
+/*
+Conditional (Ternary) Operator
+*/
+
+
+// let theAge = 30;
+
+// theAge < 20?console.log(20):theAge > 20 && theAge < 60?console.log("20 To 60"):theAge > 60?console.log("Larger Than 60"): console.log("Unknown"); 
+
